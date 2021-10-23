f1a7adf6d1ec4c2548d360451aa4dd1b02ff563f
Alexander Derevyanko <70107243+alexstark-spb@users.noreply.github.com>
2021-10-23 13:13:53 +0300

    added allure-notifications & telegram.json & simple tests

---
+++ src/test/java/com/alexstark/tests/simple/NegativeTests.java
@@ -0,0 +1,34 @@
+package com.alexstark.tests.simple;
+
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("empty")
+public class NegativeTests {
+    @Test
+    void test0() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test1() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test2() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test3() {
+        assertTrue(false);
+    }
+
+    @Test
+    void test4() {
+        assertTrue(false);
+    }
+}
---
+++ src/test/java/com/alexstark/tests/simple/SkippedTests.java
@@ -0,0 +1,28 @@
+package com.alexstark.tests.simple;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Tag("empty")
+public class SkippedTests {
+    @Test
+    @Disabled
+    void test0() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled
+    void test1() {
+        assertTrue(true);
+    }
+
+    @Test
+    @Disabled("With some reason")
+    void test2() {
+        assertTrue(true);
+    }
+}
